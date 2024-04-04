d003f5b68efd80c1ec29f72441d35caa47b976d5
bigbang123789789 <134249895+bigbang123789789@users.noreply.github.com>
2024-04-04 09:31:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import imgaug.augmenters as iaa
 import text_renderer
 from text_renderer.effect import *
 from text_renderer.corpus import *
@@ -13,16 +14,20 @@ from text_renderer.config import (
 from text_renderer.effect.curve import Curve
 
 CURRENT_DIR = Path('/content/text_renderer/workspace')
-import imgaug.augmenters as iaa
 
 text_effect_cfg = Effects([
-    text_renderer.effect.DropoutRand(p=0.1), #Drop pixel
+    text_renderer.effect.DropoutRand(p=0.1,dropout_p=(0.1, 0.2)), #Drop pixel
     Curve(p=0.2, period=180, amplitude=(7,8)), # Curve
-    Line(0.2, thickness=(2, 5),line_pos_p=(0, 1, 0, 0, 0, 0, 0, 0, 0, 0)),  #Underline
-    ImgAugEffect(p=0,aug=iaa.GaussianBlur(sigma=(0.5, 1.35))),   #Gaussian Blur
+    Line(0.1, thickness=(2, 5),line_pos_p=(0, 1, 0, 0, 0, 0, 0, 0, 0, 0)),  #Underline
+    ImgAugEffect(p=0.1,aug=iaa.GaussianBlur(sigma=(0.5, 0.85))),   #Gaussian Blur
     Padding(p=1, w_ratio=[0.015, 0.021], h_ratio=[0.3, 0.35], center=True), #Add padding
 ])
 # bg_effect_cfg = #
+
+extra_text_effect_cfg = Effects([
+    Padding(p=1, w_ratio=[0.015, 0.021], h_ratio=[0.7, 0.75], center=True), #Add padding
+])
+
 
 my_corpus = CharCorpus(
     CharCorpusCfg(
@@ -40,13 +45,13 @@ my_corpus = CharCorpus(
 
 def story_data():
     return GeneratorCfg(
-        num_image=1000000,
+        num_image=10,
         save_dir=CURRENT_DIR / "output",
         render_cfg=RenderCfg(
               corpus= [my_corpus, my_corpus],
-              corpus_effects=[text_effect_cfg, text_effect_cfg],
+              corpus_effects=[text_effect_cfg, extra_text_effect_cfg],
               bg_dir=CURRENT_DIR / "bg",
-              layout=text_renderer.layout.ExtraTextLineLayout(bottom_prob=0.05),
+              layout=text_renderer.layout.ExtraTextLineLayout(bottom_prob=0.5),
               perspective_transform=UniformPerspectiveTransformCfg(12, 12, 1.2), # rotate
               # render_effects=bg_effect_cfg
               text_color_cfg=SimpleTextColorCfg(),
